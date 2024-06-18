fd6fc6e496603d227fce1102abd7c506e9dbe0f3
Nicholas Morey <nicholas@morey.tech>
2024-06-18 02:44:24 +0200

    feat: add local argocd (#1)
    * feat: install argocd on start-up
    
    * fix: open port on cluster
    
    * fix: apps target in-cluster
    
    * fix: three nodes for ha
    
    * fix: app of apps
    
    * fix: disable app-of-apps

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+set -e  # Exit on non-zero exit code from commands
 
-echo "post-start start" >> ~/status.log
+echo "$(date): Running post-start.sh" >> ~/.status.log
 
 # this runs in background each time the container starts
 
 # Ensure kubeconfig is set up. 
 k3d kubeconfig merge dev --kubeconfig-merge-default
+kubectl apply -k argocd 2>&1 | tee -a ~/.status.log
+
+kubectl wait deployment -n argocd --all --for=condition=Available=True --timeout=90s 2>&1 | tee -a ~/.status.log
+ARGOCD_ADMIN_PASSWORD="$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+echo "${ARGOCD_ADMIN_PASSWORD}" > ~/argo-cd-admin-password.txt
+
+# kubectl apply -f app-of-apps.yaml 2>&1 | tee -a ~/.status.log
+
+argocd login \
+  "localhost:31443" \
+  --username admin \
+  --password ${ARGOCD_ADMIN_PASSWORD} \
+  --grpc-web \
+  --insecure 2>&1 | tee -a ~/.status.log
+
+echo "Argo CD admin password: ${ARGOCD_ADMIN_PASSWORD}"
 
 bash .hack/update-repo-for-workshop.sh
 
-# Best effort env load
-source ~/.bashrc
-
-echo "post-start complete" >> ~/status.log
+echo "$(date): Finished post-start.sh" >> ~/.status.log
